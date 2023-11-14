08290ca5d82e6fb129c0388e97e5ef488a98dbfe
InfraImus <73946404+InfraImus@users.noreply.github.com>
2023-11-14 15:33:40 +0100

    Add files via upload

---
+++ ResourceManager.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+//using namespace std;
+
+class ResourceManager
+{
+	ResourceManager() : res{} {}
+	
+	ResourceManager(const ResourceManager& rm) : res{rm.res} {}
+	
+	~ResourceManager()
+	{
+		delete res;
+	}
+	
+	ResourceManager& operator=(const ResourceManager& rm) 
+	{
+		delete res;
+		Resource res {rm.res};
+	}
+	
+	Resource res;
+	
+	double get()
+	{
+		return res.get();
+	}
+}
