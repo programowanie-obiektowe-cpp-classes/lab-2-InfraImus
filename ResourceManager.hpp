be026590027d8418f178a42dd4305bab278aa096
InfraImus <73946404+InfraImus@users.noreply.github.com>
2023-11-14 15:45:42 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,26 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
-};
+	ResourceManager() : res{} {}
+	
+	ResourceManager(const ResourceManager& rm) : res{rm.res} {}
+	
+	~ResourceManager()
+	{
+		delete res;
+	}
+	
+	ResourceManager& operator=(const ResourceManager& rm) 
+	{
+		delete res;
+		Resource res {rm.res};
+	}
+	
+	Resource res;
+
+public:
+	double get()
+	{
+		return res.get();
+	}
+}
